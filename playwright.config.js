88e051d61a4cfd45b34ebe25e6f34b378c1254c6
Pranav_Pothiwala <pothiwalapranav@gmail.com>
2024-01-29 18:33:45 +0000

    add merge report

--- playwright.config.js
+++ playwright.config.js
@@ -19,9 +19,9 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 10,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'blob' : 'html',
+  reporter: [['blob'],['html'],['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
